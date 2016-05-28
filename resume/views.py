0e7c0060db42f728c58161834f4d750bc472ed10
reket1990 <richard_truong@hmc.edu>
2016-05-28 10:48:17 +0000

    Added profile module and styled hero

--- resume/views.py
+++ resume/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from resume.models import *
+from datetime import date
 
 # Create your views here.
 def home(request):
 	description_entries = Description.objects.all()
 	education_entries = Education.objects.all()
 
-	return render(request, 'resume/home.html', {'description_entries': description_entries, 'education_entries': education_entries})
+	# Calculate age
+	today = date.today()
+	age = today.year - 1990 - ((today.month, today.day) < (8, 23))
+
+	return render(request, 'resume/home.html', {
+		'description_entries': description_entries,
+		'age': age,
+		'education_entries': education_entries
+	})
